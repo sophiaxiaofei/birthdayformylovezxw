1cda697aff70909b0dfd1e79f612116c8e7b270d
SophiaXiaofei <49260355+sophiaxiaofei@users.noreply.github.com>
2025-06-12 22:06:23 +0800

    Update config.js

--- config.js
+++ config.js
@@ -30,9 +30,9 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
+        turn_on: "哥哥点我",
         play: "音乐",
-        bannar_coming: "颜色",
+        bannar_coming: "哥哥快点我",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
